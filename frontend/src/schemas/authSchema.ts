b1c99bd54d22a95431ca6b067b3d89f36d9a2bf9
AmadeusTwi <amadeustwi@gmail.com>
2023-04-19 02:20:41 +0200

    adjust auth schema to match backend

--- frontend/src/schemas/authSchema.ts
+++ frontend/src/schemas/authSchema.ts
@@ -5,23 +5,19 @@ export type BasicError = {
 };
 
 export const signInSchema = z.object({
-  phone: z.string(),
   email: z.string().email("Email must be valid"),
   password: z.string().min(1, "Must not be empty"),
 });
 
 export const signInSchemaWithPhone = signInSchema.omit({ email: true });
 export type SignInFieldsWithPhone = z.infer<typeof signInSchemaWithPhone>;
-export const signInSchemaWithEmail = signInSchema.omit({ phone: true });
-export type SignInFieldsWithEmail = z.infer<typeof signInSchemaWithEmail>;
-export type SignInFields = SignInFieldsWithEmail | Record<string, never>;
+export type SignInFieldsWithEmail = z.infer<typeof signInSchema>;
 
 export const signUpSchema = z
   .object({
+    email: z.string().email("Email must be valid"),
     firstName: z.string().min(2, "First name must be at least 2 characters"),
     lastName: z.string().min(2, "Last name must be at least 2 characters"),
-    phone: z.string().min(10, "Phone must be at least 10 characters"),
-    email: z.string().email("Email must be valid"),
     password: z.string().min(8, "Password must be at least 8 characters"),
     repeatPassword: z.string(),
   })
